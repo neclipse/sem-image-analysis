d32b80c5d19a3f7b9fa0b74807e9fada366c0f36
godonan <godonanabt@gmail.cm>
2020-06-08 23:42:27 -0500

    Implemented Masks From Canny Edge Detection
    Added Gaussian blur
    Implemented three threshold calculation methods
    Implemented morphological dilation to close edges

--- src/contourDetection.py
+++ src/contourDetection.py
@@ -1,37 +1,75 @@
 import cv2
-import os
+import numpy as np
+import imutils
+
+_OTSU = "OTSU"
+_MED = "MED"
+_SAM = "SAM"
+options = [_OTSU, _MED, _SAM]
+_SIGMA = 0.33
 
 
-def detectContours(filepath):
-    org = cv2.imread(filepath)
+def detectContours(filepath, alg="MED", debug=False):
+    assert alg in options
+
     img = cv2.imread(filepath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.medianBlur(gray, 1)
-    #cv2.imshow("blur", blur)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    process = blur
+    canny = None
 
-    ret, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_OTSU)
-    cv2.imshow("thresh", thresh)
+    if debug:
+        cv2.imshow("gray", gray)
+        cv2.imshow("blur", blur)
 
-    canny = cv2.Canny(thresh, 75, 200)
-    cv2.imshow('canny', canny)
+    if alg == _OTSU:
+        upper, thresh = cv2.threshold(process, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        lower = 0.5 * upper
+        canny = cv2.Canny(thresh, lower, upper)
+        if debug:
+            cv2.imshow("thresh", thresh)
+    elif alg == _MED:
+        v = np.median(process)
+        lower = int(max(0, (1.0 - _SIGMA) * v))
+        upper = int(min(255, (1.0 + _SIGMA) * v))
+        canny = cv2.Canny(process, lower, upper)
+    elif alg == _SAM:
+        upper = 255
+        lower = 255 * 0.33
+        canny = cv2.Canny(process, lower, upper)
 
-    contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    contour_list = []
+
+    if canny is None:
+        print("Could not find canny.")
+        return None
+
+    kernel = np.ones((2, 2), np.uint8) # MODIFY THIS AS NECESSARY
+    dilation = cv2.dilate(canny, kernel, iterations=1)
+    cv2.imshow("canny", canny)
+    cv2.imshow("dilation", dilation)
+
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    mask = np.ones(img.shape[:2], dtype="uint8") * 255
+
+    posAreaContourList = []
+    print("Looping through contours")
     for contour in contours:
         approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-        # area = cv2.contourArea(contour)
-        # if 5000 < area < 15000:
-        contour_list.append(contour)
+        area = cv2.contourArea(contour)
+        #print(area)
+        if area > 20:
+            posAreaContourList.append(contour)
+            cv2.drawContours(mask, [contour], -1, (0, 255, 0), -1)
+        #cv2.imshow('mask', mask)
+        image = cv2.bitwise_and(img, img, mask=mask)
+        #cv2.imshow('after', image)
+        #cv2.waitKey(1)
 
-    msg = "Total holes: {}".format(len(approx) // 2)
-    cv2.putText(img, msg, (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
-
-    cv2.drawContours(org, contour_list, -1, (0, 255, 0), 1)
-    cv2.imshow('Objects Detected', org)
-
+    #cv2.drawContours(img, totalContourList, -1, (0, 0, 255), 1)
+    cv2.drawContours(image, posAreaContourList, -1, (0, 255, 0), 1)
+    cv2.imshow('after', image)
     cv2.waitKey(0)
+    #cv2.drawContours(mask, contours, -1, (255, 255, 255), 1)
 
-
-
-
+    #cv2.waitKey(0)
 
--- src/testRunner.py
+++ src/testRunner.py
@@ -1,15 +1,19 @@
-import blobDetection
 import os
 import contourDetection
 
 
-def main():
+def mainLoop():
     directory = r'dataset\sample'
     for subdir, dirs, files in os.walk(directory):
         for filename in files:
             filepath = subdir + os.sep + filename
             if filepath.endswith(".tif"):
                 contourDetection.detectContours(filepath)
+
+
+def main():
+    filepath = r'dataset\sample\SS 316 P&C_#1_002.tif'
+    contourDetection.detectContours(filepath, alg="MED", debug=False)
 
 
 if __name__ == "__main__":
